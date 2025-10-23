24ffdb66ce83c12fd8a199d7a9a7c724fa77af96
Grant Deken <grant@Grants-MacBook-Pro.local>
2025-10-22 22:44:55 -0400

    Add CORS to test handler

--- fastapi-backend/api/test.py
+++ fastapi-backend/api/test.py
@@ -1,7 +1,17 @@
 """Simple test handler for Vercel."""
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Add CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["https://todo-list-front-end-tau.vercel.app", "http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
